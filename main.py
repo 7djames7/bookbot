f20e252bd41cd07c63757811349809a281bb605f
7djames7 <7djames7@mgail.com>
2025-01-21 21:28:55 -0500

    Added character counting.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ all_words = []
 word_count = None
 file_path = ""
 character_count = {}
+character_list = []
+alpha_characters = {}
 
 def load_book(path):
     global raw_book
@@ -24,16 +26,50 @@ def get_characters():
     for char in book_lowercase:
         character_count[char]= character_count.get(char, 0)+1
 
+def convert_to_list(dict):
+    global character_list
+    temp_dict={}
+
+    for key in dict:
+        temp_dict={}
+        temp_dict[key] = dict[key]
+        character_list.append(temp_dict) 
+
+def get_dict_value(dict):
+  return next(iter(dict.values()))
+
+def sort_and_convert(list):
+    temp_dict= {}
+
+    list.sort(reverse=True, key=get_dict_value)
+    for dict in list:
+        temp_dict.update(dict)
+
+    return temp_dict
+
+def keep_alpha(dict):
+    global alpha_characters
+
+    for key in dict:
+        if key.isalpha():
+            alpha_characters[key] = dict[key]
+
 
 def main():
+    global character_count
     file_path = "books/frankenstein.txt"
 
     load_book(file_path)
     get_words()
-    get_characters()
-    
     print(f"The total number off words in the book is:{word_count}")
-    print(f"Here is a list of all the characters and there counts:{character_count}")
-
+    get_characters()
+    convert_to_list(character_count)
+    character_count= sort_and_convert(character_list)
+    keep_alpha(character_count)
+    for char, amount in alpha_characters.items():
+        print(f"The '{char}' character was found {amount} times.")
+    
+    
+    
     
 main()
